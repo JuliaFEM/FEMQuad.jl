33eabdb05adb364d7b08ef2403b2052cf472f249
Jukka Aho <ahojukka5@gmail.com>
2017-07-21 16:33:35 +0300

    Replace building system with PkgTestSuite.jl to make life easier (#1)

--- docs/deploy.jl
+++
@@ -1,9 +0,0 @@
-# This file is a part of project JuliaFEM.
-# License is MIT: see https://github.com/JuliaFEM/FEMQuad.jl/blob/master/LICENSE
-
-using Documenter
-using FEMQuad
-
-deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/JuliaFEM/FEMQuad.jl.git")
